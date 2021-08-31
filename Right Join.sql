1fc280b410201be07bef35126572ce4f2163b5c8
Aakif Ziauddin <57076440+AakifZ@users.noreply.github.com>
2021-08-30 22:16:43 -0500

    Delete Right Join.sql

--- Right Join.sql
+++
@@ -1,7 +0,0 @@
-SELECT register.TransactionsID,shirts.shirtID, shirts.shirtcost, register.customerID
-FROM shirts
-RIGHT JOIN register ON shirts.shirtID = register.shirtID
-ORDER BY shirts.shirtID;
-
-
-
