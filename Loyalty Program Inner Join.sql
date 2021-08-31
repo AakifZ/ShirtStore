31c752c51c9dd5a0006a27e7e5d281883badc228
Aakif Ziauddin <57076440+AakifZ@users.noreply.github.com>
2021-08-30 22:16:36 -0500

    Delete Loyalty Program Inner Join.sql

--- Loyalty Program Inner Join.sql
+++
@@ -1,3 +0,0 @@
-SELECT customer.FirstName, Customer.LastName,loyaltyprogram.customerID,loyaltyprogram.loyaltyID
-FROM Customer
-INNER JOIN loyaltyprogram ON loyaltyprogram.customerID=customer.customerID
