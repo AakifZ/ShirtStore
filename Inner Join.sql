db2bf9df860bc2c113fc22491f99ba8a72ef44ec
Aakif Ziauddin <57076440+AakifZ@users.noreply.github.com>
2021-08-30 22:15:42 -0500

    Delete Inner Join.sql

--- Inner Join.sql
+++
@@ -1,8 +0,0 @@
-SELECT online_order.online_OrderID AS OnlineOrder, shirts.shirtName AS ShirtName, online_order.shirt_color AS ShirtColor
-FROM online_order
-INNER JOIN shirts ON online_order.shirtID=shirts.ShirtID;
-
-
-
-
-
