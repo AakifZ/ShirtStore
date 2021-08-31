4d76572c61f0d269c45df8732fad393d95602d0f
Aakif Ziauddin <57076440+AakifZ@users.noreply.github.com>
2021-08-30 22:16:16 -0500

    Delete Subquery 2.sql

--- Subquery 2.sql
+++
@@ -1,6 +0,0 @@
-select customer.FirstName,customer.LastName,loyaltyprogram.customerID,loyaltyprogram.Have500points
-from customer
-right join loyaltyprogram on customer.customerID = loyaltyprogram.customerID where Have500points=true
-order by loyaltyprogram.Have500points 
-
-
