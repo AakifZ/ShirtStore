96fb93a747fc6e3ea07cf1a96363c93c0896cf83
Aakif Ziauddin <57076440+AakifZ@users.noreply.github.com>
2021-08-30 22:15:27 -0500

    Delete Broken Full Join.sql

--- Broken Full Join.sql
+++
@@ -1,5 +0,0 @@
-SELECT * from employees
-LEFT JOIN manager ON employees.employeesID = manager.employeesID
-UNION
-SELECT * from employees
-RIGHT JOIN Manager ON employees.employeesID = manager.employeesID 
